aaa3e3f0b4506480ac1e853accabce5c493d883e
harshsri28 <harshsri875@gmail.com>
2024-10-19 22:08:47 +0530

    added all crud api

--- src/main/java/com/system/student/controller/StudentController.java
+++ src/main/java/com/system/student/controller/StudentController.java
@@ -6,6 +6,8 @@ import com.system.student.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -23,5 +25,36 @@ public class StudentController {
     @GetMapping("/{id}")
     public Student getStudentInfo(@PathVariable int id) {
         return studentRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping("/all")
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+
+    @PutMapping("/update/{id}")
+    public Student updateStudentInfo(@PathVariable int id, @RequestBody Student student) {
+        Optional<Student> studentInfo = studentRepository.findById(id);
+
+        if(studentInfo.isPresent()){
+            Student studentDetails = studentInfo.get();
+            studentDetails.setName(student.getName());
+            studentDetails.setStudentId(student.getStudentId());
+            return studentRepository.save(studentDetails);
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudentInfo(@PathVariable int id) {
+        Optional<Student> studentInfo= studentRepository.findById(id);
+
+        if(studentInfo.isPresent()){
+            studentRepository.delete(studentInfo.get());
+            return "success";
+        }else{
+            return "Data not Found";
+        }
     }
 }
--- src/main/java/com/system/student/repository/StudentRepository.java
+++ src/main/java/com/system/student/repository/StudentRepository.java
@@ -3,5 +3,7 @@ package com.system.student.repository;
 import com.system.student.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// we take Integer type because in student model we  have id primary key type is integer
+// so jpa takes the type of primary key
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
